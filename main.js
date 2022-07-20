2dae9ca10c7322b4e8c983472fd09db725dfc511
Lauti2804 <els.lautaro@gmail.com>
2022-07-19 23:39:51 -0300

    Agregamos el aside y cuando tocamos el icono del carrito se muestra la lista

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 const navmenu = document.querySelector(".navbar-email");
+const burguerMenu  = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuMobile = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
 
 navmenu.addEventListener("click", toggleMenuDesktop);
+burguerMenu.addEventListener("click", toggleMenuMobile);
+menuCarritoIcon.addEventListener("click", toggleCarrito);
 
 function toggleMenuDesktop(){
     desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
+
+}
+
+function toggleMenuMobile(){
+    menuMobile.classList.toggle("inactive");
+    aside.classList.add("inactive");
+
+}
+
+function toggleCarrito(){
+    aside.classList.toggle("inactive");
+    menuMobile.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
 
 }
